555ce2eb1c7e0932ffa29d97e88754955bf4a4ee
Camilo Henao <regencamilo@gmail.com>
2024-03-26 21:06:59 -0500

    Se crea primer repositorio

---
+++ Prueba1.py
@@ -0,0 +1,8 @@
+#Lea el promedio de un alumno de la clase de programación y escriba aprobado si la nota del promedio es igual o mayor de 6
+
+promedio = float(input("Ingrese Promedio: "))
+
+if promedio >= 6:
+    print ("Aprobado")
+else: 
+    print ("Reprobado")
